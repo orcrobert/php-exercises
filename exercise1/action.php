cabffa43b370f208ee7d5504e9cc11c2a053c38c
orcrobert <robyorban@gmail.com>
2025-04-02 14:12:13 +0300

    ex 1 - completed

---
+++ exercise1/action.php
@@ -0,0 +1,90 @@
+<?php
+
+$sorted_array = array();
+
+function bubbleSort($array) {
+    $n = count($array);
+    $sorted = false;
+
+    while ($sorted === false) {
+        $sorted = true;
+
+        for ($i = 0; $i < $n - 1; $i++) {
+            if ($array[$i] > $array[$i + 1]) {
+                $sorted = false;
+                $temp = $array[$i];
+                $array[$i] = $array[$i + 1];
+                $array[$i + 1] = $temp;
+            }
+        }
+    }
+
+    return $array;
+}
+
+function selectionSort($arr) {
+    $n = count($arr);
+    for ($i = 0; $i < $n - 1; $i++) {
+        $min_index = $i;
+        for ($j = $i + 1; $j < $n; $j++) {
+            if ($arr[$j] < $arr[$min_index]) {
+                $min_index = $j;
+            }
+        }
+        $temp = $arr[$i];
+        $arr[$i] = $arr[$min_index];
+        $arr[$min_index] = $temp;
+    }
+    return $arr;
+}
+
+function insertionSort($arr) {
+    $n = count($arr);
+    for ($i = 1; $i < $n; $i++) {
+        $key = $arr[$i];
+        $j = $i - 1;
+        while ($j >= 0 && $arr[$j] > $key) {
+            $arr[$j + 1] = $arr[$j];
+            $j = $j - 1;
+        }
+        $arr[$j + 1] = $key;
+    }
+    return $arr;
+}
+
+function phpSort($arr) {
+    sort($arr);
+    return $arr;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $array = explode(",", $_POST["array"]);
+    $algorithm = $_POST["algorithm"];
+
+    for ($i = 0; $i < count($array); $i++) {
+        $array[$i] = (int) trim($array[$i]);
+    }
+
+    switch ($algorithm) {
+        case "bubble":
+            $sorted_array = bubbleSort($array);
+            break;
+        case "select":
+            $sorted_array = selectionSort($array);
+            break;
+        case "insert":
+            $sorted_array = insertionSort($array);
+            break;
+        case "php-sort":
+            $sorted_array = phpSort($array);
+            break;
+        default:
+            break;
+    }
+
+    $sortedArrayString = implode(", ", $sorted_array);
+    header("Location: index.php?sorted_array=" . $sortedArrayString);
+} else {
+    header("Location: index.php");
+}
+exit();
---
+++ exercise1/index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<form method="post" action="action.php" style="width: 300px; display: flex; flex-direction: column; gap: 10px;">
+    <label for="array">Array: </label>
+    <input type="text" id="array" name="array" required>
+
+    <label for="algorithm">Choose the sorting algorithm: </label>
+    <select name="algorithm" id="algorithm">
+        <option value="bubble">Bubble Sort</option>
+        <option value="select">Selection Sort</option>
+        <option value="insert">Insertion Sort</option>
+        <option value="php-sort">PHP Sort Function</option>
+    </select>
+
+    <button type="submit">Sort</button>
+</form>
+
+<div id="result">
+    <?php
+    if (isset($_GET['sorted_array'])) {
+        $sorted_array = explode(', ', $_GET['sorted_array']);
+
+        echo "<p><b>Sorted Array:</b> " . implode(", ", $sorted_array) . "</p>";
+    }
+    ?>
+</div>
+
+</body>
+</html>
